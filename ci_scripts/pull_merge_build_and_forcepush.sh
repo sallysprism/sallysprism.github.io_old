6a9255cb52d5983ea55c03e6ca5e1aad2771e0ff
sallysprism <33612159+sallysprism@users.noreply.github.com>
2021-10-18 17:32:00 +0900

    fix circleci to upload dist on deploy

--- ci_scripts/pull_merge_build_and_forcepush.sh
+++ ci_scripts/pull_merge_build_and_forcepush.sh
@@ -14,7 +14,7 @@ git checkout -b $DIST_PUSH_BRANCH || git checkout $DIST_PUSH_BRANCH
 mkdir -p ~/.git && git config user.email "sallysprism@gmail.com" && git config user.name "sallysprism.circleci"
 git merge -X theirs --no-edit $REPO_BRANCH
 npm run clean && npm run build
-git add -f lib
+git add -f dist
 git commit --allow-empty -m $'generated from:'"$COMMIT_HASH"$'\ntriggered by:'"$USERNAME"$'\n[ci skip]'
 git tag -a $'dist_'"$REPO_BRANCH"'_'"$BUILD_NUM" -m "."
 git push --set-upstream origin $DIST_PUSH_BRANCH --force --follow-tags
